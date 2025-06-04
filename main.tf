5a6c76b368633619d1cc4cbb48c66adc5ffe94da
bhavyar-806 <bhavyar806@gmai.com>
2025-06-04 22:14:01 +0530

    Format main.tf with terraform fmt

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "azurerm" {
-    features {}
-subscription_id = "fd240ce0-897d-4c0e-a243-662d89ccda9a"
+  features {}
+  subscription_id = "fd240ce0-897d-4c0e-a243-662d89ccda9a"
 
 }
-      
+
 resource "azurerm_resource_group" "git-rg" {
   name     = var.resource_group_name
   location = var.location
-}      
+}
 
 resource "azurerm_kubernetes_cluster" "aks-git" {
   name                = var.aks_cluster_name
